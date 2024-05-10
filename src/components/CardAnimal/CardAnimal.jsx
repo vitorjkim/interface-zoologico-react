fcaa7d37e73dcbf257f49c4a386f2cabf7ce897c
Vitor Joaquim <vitoreojoaquim@gmail.com>
2024-05-10 09:12:16 -0300

    adicionando os cards nos OS ANIMAIS

--- src/components/CardAnimal/CardAnimal.jsx
+++ src/components/CardAnimal/CardAnimal.jsx
@@ -1,15 +1,21 @@
-import imgNotFound from '../../assets/img-not-found.png'
 import './CardAnimal.css'
+import imgNotFound from '../../assets/img-not-found.png'
 
-function CardAnimal() {
+function CardAnimal({ animal }) {
+
+    const exibeID = () => {
+        console.log(animal.idanimal, "\n", animal);
+    }
+
     return (
-        <>
-            <div className="ctn-cardanimal">
-                <img src={imgNotFound} />
-                <p>Lorem Ipsum</p>
-            </div>
-        </>
+        <div className='card-animal'>
+            <img src={imgNotFound} alt="Imagem não encontrada" onClick={exibeID} />
+            <p>Nome: {animal.nomeanimal}</p>
+            <p>Gênero: {animal.generoanimal}</p>
+            <p>Espécie: {animal.tipoanimal}</p>
+            <p>Envergadura: {animal.envergadura}</p>
+        </div >
     );
 }
 
-export default CardAnimal
+export default CardAnimal;
--- src/pages/Animais/Animais.jsx
+++ src/pages/Animais/Animais.jsx
@@ -1,14 +1,44 @@
-import Navegacao from '../../components/Navegacao/Navegacao';
+import React, { useEffect, useState } from 'react';
 import CardAnimal from '../../components/CardAnimal/CardAnimal';
-import './Animais.css'
+import Navegacao from '../../components/Navegacao/Navegacao';
+import './Animais.css'; // Importe o arquivo CSS aqui
 
 function Animais() {
-    return(
+    const [animais, setAnimais] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/listar-aves');
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar servidor');
+                }
+                const listaAnimais = await response.json();
+                setAnimais(listaAnimais);
+            } catch (error) {
+                console.error('Erro ao buscar animais:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
         <>
             <Navegacao />
-            <CardAnimal />
+     
+            <div className='ctn-animais'>
+                {animais ? (
+                    // Renderize o componente CardAnimal para cada item da lista
+                    animais.map(animal => (
+                        <CardAnimal key={animal.idanimal} animal={animal} />
+                    ))
+                ) : (
+                    <p>Carregando... verifique se o servidor está funcionando</p>
+                )}
+            </div>
         </>
     );
 }
 
-export default Animais
+export default Animais;
